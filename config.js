08cd55a3b02944420ee087f4da9f3f794460f37d
staporn123 <Sataporn282537@gmail.com>
2026-06-09 15:18:38 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbyhfY5hnfYthiIEc7k7Y1tZXNkacYHU_Jav9xjOLDefqteHvg6_MyTXedRNG8Y__RN3/exec",
+  DOC_PREFIX: "ฉ.1 กฟส.ออ.(ปร)-",
+  VERSION: "4.0.0"
+};
