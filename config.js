7a31532ab2b18b075553e8305a0ab989f7f0c23c
staporn123 <Sataporn282537@gmail.com>
2026-06-10 00:08:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbzD-8Y0RkLGy4zYM7xe8P5g7gWyDh_UPD9BWnZh4WQz4LU_4VNXCtLKqiAdEbxK2VOH/exec",
   DOC_PREFIX: "ฉ.1 กฟส.ออ.(ปร)-",
-  VERSION: "4.0.0"
+  VERSION: "4.0.5"
 };
