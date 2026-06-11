95354f60157165151189e70782056b09b2ec7063
staporn123 <Sataporn282537@gmail.com>
2026-06-11 13:33:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxnntRIvB9df-yANZmQNwhtQGRC_rhr4jc-fRCQpCZ5dTEe7ltek0NbbRgQgISwR6Y2/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyt5z6xZUt3eW-otQjP82Gfx19AOO5DKhJjWgOu7nHI0vaU2u5IgJ1hokhot16fXo6f/exec",
   DOC_PREFIX: "ฉ.1 กฟส.ออ.(ปร)-",
-  VERSION: "4.0.7"
+  VERSION: "4.1.1"
 };
